5894c42ea2cf8822574ac68df823cc3a4ce6ed94
sundongzhe <sundongzhe@kuaishou.com>
2025-05-27 10:49:35 +0800

    recovery

--- main.py
+++ main.py
@@ -39,45 +39,45 @@ def main():
     args = parser.parse_args()
 
     # 开始处理
-    # update_status(f"Start! {args.video}")
-    # config = load_config()
+    update_status(f"Start! {args.video}")
+    config = load_config()
     file_name, ext = os.path.splitext(args.video)
     fps = detect_fps(args.video)
 
-    # # 提取视频帧
-    # update_status(f"Source: extracting frames with {fps} FPS...")
-    # # extract_frames(args.video, fps)
-    # temp_directory_path = get_temp_directory_path(args.video)
-    # frame_paths = get_temp_frame_paths(temp_directory_path)
+    # 提取视频帧
+    update_status(f"Source: extracting frames with {fps} FPS...")
+    extract_frames(args.video, fps)
+    temp_directory_path = get_temp_directory_path(args.video)
+    frame_paths = get_temp_frame_paths(temp_directory_path)
 
-    # # 使用 OCR 提取字幕
-    # update_status("OCR: extracting subtitles...")
-    # ocr_result, y_center = extract_subtitles(frame_paths, config, fps)
+    # 使用 OCR 提取字幕
+    update_status("OCR: extracting subtitles...")
+    ocr_result, y_center = extract_subtitles(frame_paths, config, fps)
 
-    # # 生成字幕
-    # srt_path = get_subtitles(ocr_result, config, fps, file_name)
+    # 生成字幕
+    srt_path = get_subtitles(ocr_result, config, fps, file_name)
 
-    # # 擦除原有字幕
-    # update_status("Erase: removing subtitles...")
-    # remove_subtitles(ocr_result, fps, len(frame_paths), config)
+    # 擦除原有字幕
+    update_status("Erase: removing subtitles...")
+    remove_subtitles(ocr_result, fps, len(frame_paths), config)
     output_path = f"{file_name}_output{ext}"
     print("output_path")
     print(output_path)
     create_video(args.video, output_path, fps)
 
-    # # 翻译字幕
-    # update_status("Translate: translating subtitles...")
-    # srt_lang_path = translate_subtitles(srt_path, args.language)
+    # 翻译字幕
+    update_status("Translate: translating subtitles...")
+    srt_lang_path = translate_subtitles(srt_path, args.language)
 
-    # # 将翻译后的字幕嵌入视频
-    # update_status("Embed: embedding subtitles...")
-    # output_file = f"{file_name}_{args.language}{ext}"
-    # embed_subtitles(output_path, srt_lang_path, y_center, output_file, config)
+    # 将翻译后的字幕嵌入视频
+    update_status("Embed: embedding subtitles...")
+    output_file = f"{file_name}_{args.language}{ext}"
+    embed_subtitles(output_path, srt_lang_path, y_center, output_file, config)
 
-    # if args.delete:
-    #     if os.path.exists(file_name):
-    #         shutil.rmtree(file_name)
-    #         update_status("Temporary request directory {} deleted".format(file_name))
+    if args.delete:
+        if os.path.exists(file_name):
+            shutil.rmtree(file_name)
+            update_status("Temporary request directory {} deleted".format(file_name))
 
     update_status(f"Done! {args.video}")
 
